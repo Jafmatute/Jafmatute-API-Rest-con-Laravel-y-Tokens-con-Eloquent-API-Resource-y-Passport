8426ffff6be6728e3acc6c6ae3c779f06a06e104
Jafmatute <Jafmatute@gmail.com>
2020-08-31 03:52:20 -0500

    configuration of passport auth and creation of API RESOURCE for locations, users and customers

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\LocationResource;
+use App\Location;
 use Illuminate\Http\Request;
 
 class LocationController extends Controller
 {
-    //
+    public function index()
+    {
+        return LocationResource::collection(Location::all());
+    }
 }
